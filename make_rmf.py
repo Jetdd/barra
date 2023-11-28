560707517e58b8754a045cfad8d43877f938bfeb
Jetdd <dengzhenleih@gmail.com>
2023-11-28 16:50:12 +0800

    1

--- make_rmf.py
+++ make_rmf.py
@@ -8,6 +8,7 @@ import pandas as pd
 from config import read_path, dominant_path, contracts_path
 import numpy as np
 
+
 def get_close(tp: str) -> pd.DataFrame:
     """根据品种得到Concated DataFrame of the CLOSE
 
@@ -28,6 +29,7 @@ def get_close(tp: str) -> pd.DataFrame:
 
     return new_df
 
+
 def get_contracts_template(tp: str) -> pd.DataFrame:
     """根据品种从米筐动态获得当日可交易合约列表, 通过主力合约筛选掉近月到期合约, 再将可交易合约组成RMF (BARRA COM2) 矩阵
 
@@ -38,12 +40,16 @@ def get_contracts_template(tp: str) -> pd.DataFrame:
         pd.DataFrame: columns=[rmf_1, rmf_2, ..., rmf_10], index=date, values=contract
     """
 
-    dominant_df = pd.read_csv(dominant_path / "{}.csv".format(tp)).set_index('date')
-    contracts_df = pd.read_csv(contracts_path / "{}.csv".format(tp)).rename(columns={"Unnamed: 0":'date'}).set_index('date')
+    dominant_df = pd.read_csv(dominant_path / "{}.csv".format(tp)).set_index("date")
+    contracts_df = (
+        pd.read_csv(contracts_path / "{}.csv".format(tp))
+        .rename(columns={"Unnamed: 0": "date"})
+        .set_index("date")
+    )
     new_contracts = []
     # 遍历全部合约的序列, 去掉比主力更近的近月合约
     for _date in dominant_df.index:
-        dominant_contract_num = dominant_df['dominant'].loc[_date][-4:]
+        dominant_contract_num = dominant_df["dominant"].loc[_date][-4:]
         daily_contracts = contracts_df.loc[_date]
         selected_contracts = [
             con
@@ -56,7 +62,8 @@ def get_contracts_template(tp: str) -> pd.DataFrame:
     template = pd.DataFrame(index=dominant_df.index, data=new_contracts)
     template.columns = [f"rmf_{i+1}" for i in range(len(template.columns))]
 
-    return template.iloc[:-1] # 用来对齐价格数据, 价格数据暂未更新到最后一天
+    return template.iloc[:-1]  # 用来对齐价格数据, 价格数据暂未更新到最后一天
+
 
 def make_rmf(tp: str) -> pd.DataFrame:
     """制作RMF矩阵, 将TEMPLATE填充CLOSE价格, 并做换月平滑
@@ -69,23 +76,65 @@ def make_rmf(tp: str) -> pd.DataFrame:
     """
     template = get_contracts_template(tp=tp)
     close_df = get_close(tp=tp)
-    
-    # Mapping close prices
-    rmf_list = []
-    for date, row in template.iterrows():
-        row = row.dropna()
-        daily_close = close_df[row].loc[date]
-        rmf_list.append(daily_close.values)
 
-    
-    rmf_df = pd.DataFrame(data=rmf_list, index=template.index)
+    # Mapping close prices
+    dfs = []
+    for col in template.columns:
+        contract_series = template[col]
+        mapped_close_prices = map_close_prices(
+            contract_series=contract_series, close_price_df=close_df
+        )
+        dfs.append(mapped_close_prices)
+
+    rmf_df = pd.concat(dfs, axis=1)
+    rmf_df = rmf_df.dropna(axis=1)  # 去除有nan值的rmf列
     rmf_df.columns = [f"rmf_{i+1}" for i in range(len(rmf_df.columns))]
-    
+
     # Smoothing close prices
-    
+    # 平滑规则, 换合约前4天按照4/5前合约, 1/5后合约, ...的规律进行平滑拼接
+    for col in rmf_df.columns:
+        prev_1_index = np.where(template[col] != template[col].shift(-1))[
+            0
+        ]  # 合约进行换月切换, 下一天为新合约
+
+        # 循环平滑
+        for i in range(4):
+            prev_i = prev_1_index - i  # 切换前i+1天, 新合约切换前2天也就是i=1
+            outcoming_contracts = template[col][prev_i]  # 旧合约
+            incoming_contracts = template[col].shift(-i - 1)[
+                prev_i
+            ]  # 新合约在换月前i天, index需要往前i+1天
+            outcoming_prices = map_close_prices(
+                contract_series=outcoming_contracts, close_price_df=close_df
+            )
+            incoming_prices = map_close_prices(
+                contract_series=incoming_contracts, close_price_df=close_df
+            )
+            # Update
+            rmf_df[col][prev_i] = (i + 1) / 5 * outcoming_prices + (
+                4 - i
+            ) / 5 * incoming_prices
+
     return rmf_df
 
-template = get_contracts_template('AG')
-close_df = get_close('AG')
-make_rmf('AG')
 
+def map_close_prices(
+    contract_series: pd.Series, close_price_df: pd.DataFrame
+) -> pd.Series:
+    """UTIL FUNCTION: 按照df里存下的合约template进行close prices projections
+
+    Args:
+        contract_series (pd.Series): index=date, values=contracts
+        close_price_df (pd.DataFrame): close_df obtained from get_close
+
+    Returns:
+        pd.Series: index=date, values=close
+    """
+    # Map each contract to its corresponding close price for each date
+    mapped_close_prices = contract_series.index.to_series().map(
+        lambda date: close_price_df.at[date, contract_series[date]]
+        if contract_series[date] in close_price_df.columns
+        else np.nan
+    )
+
+    return mapped_close_prices
